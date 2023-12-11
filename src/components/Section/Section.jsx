087e811a7c46dc3cb0c8dcd26738fc5a2a3eca12
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2023-12-11 21:41:54 +0200

    Initial commit

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './Section.module.css';
+
+export const Section = ({ title, children }) => {
+  return (
+    <section className={s.section}>
+      <h2 className={s.title}>{title}</h2>
+      {children}
+    </section>
+  );
+};
